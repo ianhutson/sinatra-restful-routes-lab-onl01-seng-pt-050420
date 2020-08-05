305702f4facc07e2c5af3a9a818665485106c986
Ian Hutson <ih00209@georgiasouthern.edu>
2020-08-05 17:52:08 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,14 @@ class ApplicationController < Sinatra::Base
   erb :index
 end
 
+get '/articles/new' do
+  erb :new
+end
+ 
+post '/articles' do
+  @article = Article.create(:title => params[:title], :content => params[:content])
+  redirect to "/articles/#{@article.id}"
+end
 
 
 end
